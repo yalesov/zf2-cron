87277b250abfd1e888a419f28cfb32abc582d3df
heartsentwined <heartsentwined@cogito-lab.com>
2012-09-18 12:49:35 +0800

    bugfixes

--- src/Cron/Module.php
+++ src/Cron/Module.php
@@ -16,9 +16,6 @@ class Module implements AutoloaderProviderInterface
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__,
